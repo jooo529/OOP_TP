e90514735c0fe182db37ece8eba42e336d7d8cca
201520974 <cjy10_01@naver.com>
2016-05-22 23:59:29 +0900

    asd

--- TP/src/FarmingFrame.java
+++ TP/src/FarmingFrame.java
@@ -2,7 +2,8 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.Graphics;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.*;
 import javax.swing.*;
 
@@ -12,48 +13,33 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 
-public class FarmingFrame extends JFrame{
+public class FarmingFrame extends JFrame {
 
-	public FarmingFrame(){
+	public FarmingFrame() {
 		go_farming();
 	}
+
 	ImageIcon im;
 	Container container;
 	JList<String> list;
-	
-	public void go_farming() {	//선택받은 과일이 입력받은 갯수만큼 나타나는거 구현하기
+
+	public void go_farming() { // 선택받은 과일이 입력받은 갯수만큼 나타나는거 구현하기
 
 		container = this.getContentPane();
 		container.setLayout(new BorderLayout());
 
-		im = new ImageIcon("farm.png");
+		im = new ImageIcon("first.png");
 		JPanel back_ground = new JPanel() {
 			public void paintComponent(Graphics g) {
 				g.drawImage(im.getImage(), 0, 0, null);
 
-				g.setFont(new Font("serif", Font.BOLD, 28));
-				g.drawString("test", 25, 35);
 			}
 		};
-
-		JButton gochoose = new JButton("Go to choose");
-		gochoose.setFont(new Font("serif", Font.BOLD, 20));
-//		gochoose.addActionListener(new gochoose_listener());
-
-		back_ground.add(gochoose);
-
-		container.add(BorderLayout.CENTER, back_ground);
-		// *
-
-		// *
-
-		JButton goback = new JButton("back to main");
-//		goback.addActionListener(new goback_listener());
-
+		
 		JPanel menupanel = new JPanel();
-
-		container.add(BorderLayout.NORTH, menupanel);
-
+		
+		container.add(BorderLayout.NORTH,menupanel);
+	
 		User u = new User();
 		Font font = new Font("Dialog", Font.BOLD, 15); // 20은 글자 크기
 
@@ -72,8 +58,33 @@ public class FarmingFrame extends JFrame{
 		menupanel.add(userLEVEL_label);
 		menupanel.add(userMONEY_label);
 		menupanel.add(enter);
-		menupanel.add(goback);
+//		menupanel.add(goback);
 
+		JPanel fruitpanel = new JPanel();
+		fruitpanel.setLayout(new GridLayout(4,4));
+		
+		container.add(BorderLayout.CENTER,fruitpanel);
+		
+		for (int i = 0; i < 16; i++) {
+			JButton btn = new JButton(){
+				public void paintComponent(Graphics g){
+					g.drawImage(im.getImage(), 0,0,null);
+				}
+			};
+			fruitpanel.add(btn);
+		}
+		
+		JButton backbtn = new JButton("Quit Farming");
+		backbtn.addActionListener(new quit_listener());
+		
+		container.add(BorderLayout.SOUTH,backbtn);
+	}
+	
+	class quit_listener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			
+		}
 	}
 
+	
 }
