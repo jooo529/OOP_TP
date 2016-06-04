aa18d46640bbfcffad738eafa4b8beed906ff411
jooo529 <jooo529@ajou.ac.kr>
2016-06-05 02:08:01 +0900

    ss

--- TP/src/MainFrame.java
+++ TP/src/MainFrame.java
@@ -2,11 +2,11 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-///import StoreFrame.goback_listener;
-
 public class MainFrame extends JFrame{
 
 	UserFile f = new UserFile();
+	MissionFile mf = new MissionFile();
+	static int idx = -1;
 	
 	public MainFrame(){
 		go_main();
@@ -15,7 +15,6 @@ public class MainFrame extends JFrame{
 	ImageIcon im;
 	
 	public String toUserInfo() {
-        int idx = -1;
         for(int i=0; i<UserFile.Users.size(); i++){
         	if(UserFile.Users.get(i).getIndex()>-1) idx = i;
 		}		
@@ -64,17 +63,24 @@ public class MainFrame extends JFrame{
 		store_button.setBounds(585, 150, 100, 100);
 		store_button.addActionListener(new StoreButtonListener());
 		
-		JTextArea missionInfo = new JTextArea("Mission\n\n\n\n\n\n");
-		missionInfo.setBounds(735, 48, 250,75);
+		JTextArea missionInfo = new JTextArea();
+		missionInfo.setBounds(735, 48, 250, 250);
+		missionInfo.setEditable(false);
 
-		JTextField userInfo = new JTextField(toUserInfo(),JLabel.CENTER);
+		JTextField userInfo = new JTextField(toUserInfo(), JLabel.CENTER);
 		Font font = new Font("Dialog", Font.BOLD, 14);
 		userInfo.setFont(font);
 		userInfo.setEditable(false);
-		
 		userInfo.setBounds(633, 13, 352, 30);
 		userInfo.setBackground(Color.BLACK);
 		userInfo.setForeground(Color.ORANGE);
+
+		if (UserFile.Users.get(idx).getLevel() == 0)	missionInfo.setText(mf.fileLoad("Mission0.txt"));
+		else if (UserFile.Users.get(idx).getLevel() == 1)	missionInfo.setText(mf.fileLoad("Mission1.txt"));
+		else if (UserFile.Users.get(idx).getLevel() == 2)	missionInfo.setText(mf.fileLoad("Mission2.txt"));
+		else if (UserFile.Users.get(idx).getLevel() == 3)	missionInfo.setText(mf.fileLoad("Mission3.txt"));
+		else if (UserFile.Users.get(idx).getLevel() == 4)	missionInfo.setText(mf.fileLoad("Mission4.txt"));
+		else if (UserFile.Users.get(idx).getLevel() == 5)	missionInfo.setText(mf.fileLoad("Mission5.txt"));
 		
 		JButton quit_button = new JButton("Save and Quit");
 		quit_button.setBounds(735, 450, 250, 80);
