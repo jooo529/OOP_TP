3ad1a335d4f76afc8b51fe05fce20483445d543b
jooo529 <jooo529@ajou.ac.kr>
2016-06-05 02:17:46 +0900

    1

--- TP/src/HomeFrame.java
+++ TP/src/HomeFrame.java
@@ -6,44 +6,44 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class HomeFrame extends JFrame{
-	
-	public HomeFrame(){
+public class HomeFrame extends JFrame {
+
+	public HomeFrame() {
 		go_home();
 	}
+
+	UserFile f = new UserFile();
 	BufferedImage im_background;
 	private ImageIcon tb;
 	JButton Exit = new JButton("Exit");
 	JButton character = new JButton();
 	Container container;
-	//
+	static int idx = -1;
 	
-	public void go_home()
-	{
+	public void go_home() {
 		try {
 			im_background = ImageIO.read(new File("HOME3.png"));
-			
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		tb = new ImageIcon("textBallon_click.png");
-        container = this.getContentPane();
-       
-		JPanel back_ground = new JPanel(){ 
-		 public void paintComponent(Graphics g) {
-            
-			 g.drawImage(im_background, 0, 0, 1000, 550, this);
-			 g.drawImage(tb.getImage(), 600, 150, null);
-	
-        }};
-      
-       back_ground.setLayout(null);
-       container.add(BorderLayout.CENTER,back_ground);
-       
-       Exit = new JButton("Exit");
-       
-		
+		container = this.getContentPane();
+
+		JPanel back_ground = new JPanel() {
+			public void paintComponent(Graphics g) {
+
+				g.drawImage(im_background, 0, 0, 1000, 550, this);
+				g.drawImage(tb.getImage(), 600, 150, null);
+
+			}
+		};
+
+		back_ground.setLayout(null);
+		container.add(BorderLayout.CENTER, back_ground);
+
+		Exit = new JButton("Exit");
+
 		Exit.setContentAreaFilled(false);
 		Exit.setDefaultCapable(false);
 		Exit.setFocusPainted(false);
@@ -56,8 +56,7 @@ public class HomeFrame extends JFrame{
 
 		Exit.setBounds(900, 400, 60, 60);
 		Exit.addActionListener(new button_listener());
-		
-		
+
 		character.setContentAreaFilled(false);
 		character.setDefaultCapable(false);
 		character.setFocusPainted(false);
@@ -66,25 +65,21 @@ public class HomeFrame extends JFrame{
 		character.setRolloverIcon(new ImageIcon("mom_stand_c.png"));
 		character.setBorderPainted(false);
 		character.addActionListener(new button_listener());
-		character.setBounds(550, 150, 110 ,300 );		
+		character.setBounds(550, 150, 110, 300);
 		character.setOpaque(false);
-		
+
 		back_ground.add(character);
 		back_ground.add(Exit);
-		
-		
-		
-		
-        JPanel menupanel = new JPanel();
 
-		User u = new User();
-		Font font = new Font("Dialog",Font.BOLD, 15); //20은 글자 크기
-		
-        JButton goback = new JButton("back to main");
-		
-        int idx=-1;
-        for(int i=0; i<UserFile.Users.size(); i++){
-			if(UserFile.Users.get(i).getIndex()>-1) idx = i;
+		JPanel menupanel = new JPanel();
+
+		Font font = new Font("Dialog", Font.BOLD, 15); // 20은 글자 크기
+
+		JButton goback = new JButton("back to main");
+
+		for (int i = 0; i < UserFile.Users.size(); i++) {
+			if (UserFile.Users.get(i).getIndex() > -1)
+				idx = i;
 		}
 		JLabel userID_label = new JLabel("U s e r   :   " + UserFile.Users.get(idx).getId());
 		userID_label.setFont(font);
@@ -94,57 +89,35 @@ public class HomeFrame extends JFrame{
 
 		JLabel userLEVEL_label = new JLabel("           L e v e l   :   " + UserFile.Users.get(idx).getLevel());
 		userLEVEL_label.setFont(font);
-		
+
 		JLabel enter = new JLabel("                      ");
-		
+
 		menupanel.add(userID_label);
 		menupanel.add(userLEVEL_label);
 		menupanel.add(userMONEY_label);
 		menupanel.add(enter);
-		//menupanel.add(Exit);
-		container.add(BorderLayout.NORTH,menupanel);
-		
-		
+		// menupanel.add(Exit);
+		container.add(BorderLayout.NORTH, menupanel);
+
 	}
-	
-	class button_listener implements ActionListener
-	{
-		public void actionPerformed(ActionEvent event)
-		{
-			
-			if(event.getSource()==character)
-			{
+
+	class button_listener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+
+			if (event.getSource() == character) {
+				if(UserFile.Users.get(idx).getLevel()==0){
+					JOptionPane.showMessageDialog(null, "You met Mom! LEVEL UP!", "Message", JOptionPane.INFORMATION_MESSAGE);
+					UserFile.Users.get(idx).setLevel(1);
+					f.fileSave();
+				}
 				NpcDisplay npc = new NpcDisplay();
 				npc.go();
-			}
-			else if(event.getSource()==Exit)
-			{
+			} else if (event.getSource() == Exit) {
 				setVisible(false);
 			}
-			
+
 		}
 	}
-	
-	/*class mouse_listener extends MouseAdapter
-	{
-
-		@Override
-		public void mouseClicked(MouseEvent e){
-			//System.out.println(e.getPoint().x +"and"+e.getPoint().y);
-			if(e.getPoint().x>450&&e.getPoint().x<580&&e.getPoint().y>275&&e.getPoint().y<410){
-				if(e.getClickCount() == 2){
-					NpcDisplay npc = new NpcDisplay();
-					npc.go();
-					
-				}
-				
-			}
-			
-		}
-
-
-	}*/
-	
 
 
 }
