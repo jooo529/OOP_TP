5db926ee27baa600a9ab0d8d457a561878e1328f
chogeonhee <coverstopy@naver.com>
2016-06-07 00:03:20 +0900

    home checkmission getmission npc help 코드정리 및 display클래스 삭제

--- TP/src/HomeFrame.java
+++ TP/src/HomeFrame.java
@@ -9,41 +9,68 @@ import javax.swing.*;
 public class HomeFrame extends JFrame {
 
 	public HomeFrame() {
-		go_home();
+		//set frame's state
+		this.setSize(1000,590);
+		this.setTitle("Farming Game");
+		this.setVisible(true);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(LoginFrame.DISPOSE_ON_CLOSE);
+		
+		//entire window's size
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		  
+		//locate frame in center
+		int xPos = screenSize.width/2 - this.getSize().width/2 ;
+		int yPos = screenSize.height/2 - this.getSize().height/2 ;
+
+		this.setLocation(xPos,yPos);
+		
+		this.go_home();
 	}
 
-	UserFile f = new UserFile();
-	BufferedImage im_background;
+	private UserFile f = new UserFile();
+	private BufferedImage im_background;
 	private ImageIcon tb;
-	JButton Exit = new JButton("Exit");
-	JButton character = new JButton();
-	Container container;
+	private JButton Exit = new JButton("Exit");
+	private JButton character = new JButton();
+	private Container container;
+	private JPanel back_ground;
+	private JPanel menupanel;
+	private JButton goback;
+	
+	private Font font = new Font("Dialog", Font.BOLD, 15); // 20은 글자 크기
+	
 	static int idx = -1;
 	
 	public void go_home() {
+		
+		container = this.getContentPane();
+		
 		try {
-			im_background = ImageIO.read(new File("HOME3.png"));
+			im_background = ImageIO.read(new File("HOME3.png")); //home background
 
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (IllegalArgumentException e2){
+			e2.printStackTrace();
 		}
-		tb = new ImageIcon("textBallon_click.png");
-		container = this.getContentPane();
+		
+		
+		tb = new ImageIcon("textBallon_click.png"); //NPC in home says
+	
 
-		JPanel back_ground = new JPanel() {
+		back_ground = new JPanel() { //set image in Home
 			public void paintComponent(Graphics g) {
-
 				g.drawImage(im_background, 0, 0, 1000, 550, this);
 				g.drawImage(tb.getImage(), 600, 150, null);
-
 			}
 		};
 
 		back_ground.setLayout(null);
 		container.add(BorderLayout.CENTER, back_ground);
 
+		//to make image's background transparent, reference ->http://laonatti.net/196
 		Exit = new JButton("Exit");
-
 		Exit.setContentAreaFilled(false);
 		Exit.setDefaultCapable(false);
 		Exit.setFocusPainted(false);
@@ -53,7 +80,6 @@ public class HomeFrame extends JFrame {
 		Exit.setRolloverIcon(new ImageIcon("button_exit_s.png"));
 		Exit.setBackground(null);
 		Exit.setBorderPainted(false);
-
 		Exit.setBounds(900, 400, 60, 60);
 		Exit.addActionListener(new button_listener());
 
@@ -68,19 +94,25 @@ public class HomeFrame extends JFrame {
 		character.setBounds(550, 150, 110, 300);
 		character.setOpaque(false);
 
-		back_ground.add(character);
-		back_ground.add(Exit);
-
-		JPanel menupanel = new JPanel();
-
-		Font font = new Font("Dialog", Font.BOLD, 15); // 20은 글자 크기
-
-		JButton goback = new JButton("back to main");
-
-		for (int i = 0; i < UserFile.Users.size(); i++) {
-			if (UserFile.Users.get(i).getIndex() > -1)
-				idx = i;
+		try{
+			back_ground.add(character);
+			back_ground.add(Exit);
 		}
+		catch(NullPointerException e){
+			e.printStackTrace();
+		}
+		
+		try{
+			for (int i = 0; i < UserFile.Users.size(); i++) {
+				if (UserFile.Users.get(i).getIndex() > -1)
+					idx = i;
+			}
+		}
+		catch(IndexOutOfBoundsException e)
+		{
+			e.printStackTrace();
+		}
+		
 		JLabel userID_label = new JLabel("U s e r   :   " + UserFile.Users.get(idx).getId());
 		userID_label.setFont(font);
 
@@ -92,31 +124,44 @@ public class HomeFrame extends JFrame {
 
 		JLabel enter = new JLabel("                      ");
 
-		menupanel.add(userID_label);
-		menupanel.add(userLEVEL_label);
-		menupanel.add(userMONEY_label);
-		menupanel.add(enter);
-		// menupanel.add(Exit);
-		container.add(BorderLayout.NORTH, menupanel);
+		menupanel = new JPanel();
+		try{
+			menupanel.add(userID_label);
+			menupanel.add(userLEVEL_label);
+			menupanel.add(userMONEY_label);
+			menupanel.add(enter);
+			// menupanel.add(Exit);
+			container.add(BorderLayout.NORTH, menupanel);
+		}
+		catch(NullPointerException e){
+			e.printStackTrace();
+		}
 
 	}
 
 	class button_listener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 
-			if (event.getSource() == character) {
-				if(UserFile.Users.get(idx).getLevel()==0){
-					JOptionPane.showMessageDialog(null, "You met Mom! LEVEL UP!", "Message", JOptionPane.INFORMATION_MESSAGE);
-					UserFile.Users.get(idx).setLevel(1);
+			try{
+				if (event.getSource() == character) {
+					if(UserFile.Users.get(idx).getLevel()==0){
+						JOptionPane.showMessageDialog(null, "You met Mom! LEVEL UP!", "Message", JOptionPane.INFORMATION_MESSAGE);
+						UserFile.Users.get(idx).setLevel(1);
+						f.fileSave();
+					}
+					NpcFrame np = new NpcFrame();
+				} else if (event.getSource() == Exit) {
 					f.fileSave();
+					setVisible(false);
+					MainDisplay md = new MainDisplay();
+					md.go();
 				}
-				NpcDisplay npc = new NpcDisplay();
-				npc.go();
-			} else if (event.getSource() == Exit) {
-				f.fileSave();
-				setVisible(false);
-				MainDisplay md = new MainDisplay();
-				md.go();
+			}
+			catch(IndexOutOfBoundsException e){
+				e.printStackTrace();	
+			}
+			catch(HeadlessException e2){
+				e2.printStackTrace();
 			}
 
 		}
--- TP/src/checkFrame.java
+++ TP/src/checkFrame.java
@@ -14,170 +14,245 @@ import javax.swing.*;
 
 public class checkFrame extends JFrame {
 
-	public checkFrame() {
-		go_cf();
+	public checkFrame(){
+		//set frame's state
+		this.setSize(700,350);
+		this.setTitle("Check Mission");
+		this.setVisible(true);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(LoginFrame.DISPOSE_ON_CLOSE);
+			
+			//entire window's size
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			  
+			////locate frame in center
+		int xPos = screenSize.width/2 - this.getSize().width/2 ;
+		int yPos = screenSize.height/2 - this.getSize().height/2 ;
+
+		this.setLocation(xPos,yPos);
+			
+		
+		
+		this.go_cf();
 	}
 
-	private Container container;
-
-	private ImageIcon npcf;
-	private ImageIcon tb;
-	private JPanel window;
-	private JPanel checkmission;
-	private JTextArea mission;
-	private JButton checkAndexit;
-	Font font2 = new Font("Kristen ITC", Font.BOLD, 14);
-
-	JButton mission1;
-	JButton mission2;
-	JButton mission3;
-	JButton mission4;
-	JButton mission5;
-	Font font = new Font("Kristen ITC", Font.BOLD, 20); // 30글자 크기
+	private Container container; //Frame
 	
-	UserFile f = new UserFile();
+	private ImageIcon npcf; //Mom NPC's image
+	private ImageIcon tb; //Mom's comment
+	
+	private JPanel window; // It's MISSION 1~5 <- button
+	private JPanel checkmission; //if you click one of window's button, show this panel.
+	private JPanel npc_face; 
+	private JTextArea mission; // text of mission checking
+	private JButton checkAndexit; // check and exit
+	
+	private Font font2 = new Font("Kristen ITC", Font.BOLD, 14);
+	private Font font = new Font("Kristen ITC", Font.BOLD, 20); // 30글자 크기
+	
+	private JButton mission1;
+	private JButton mission2;
+	private JButton mission3;
+	private JButton mission4;
+	private JButton mission5;
+	
+	private JLabel label1;
+	private JLabel label2;
+	private JLabel label3;
+	private JLabel label4;
+	private JLabel label5;
+	
+	private Border missionBorder;
+	private Border emptyBorder;
+	
+	private UserFile f = new UserFile();
 	static int idx = -1;
 	static Mission[] m = new Mission[5];
 	
+	
+	
 	void go_cf() {
 
-		for (int i = 0; i < UserFile.Users.size(); i++) {
-			if (UserFile.Users.get(i).getIndex() > -1)
-				idx = i;
+		try{
+			for (int i = 0; i < UserFile.Users.size(); i++) {
+				if (UserFile.Users.get(i).getIndex() > -1) //throw exception
+					idx = i;
+			} //id (who's?)
 		}
 		
-		Font font_italic = new Font("Serif", Font.ITALIC + Font.BOLD, 14);
-		container = this.getContentPane();
-		container.setLayout(null);
+		catch(IndexOutOfBoundsException e)
+		{
+			e.printStackTrace();
+		}
 
-		window = new JPanel();// 여기에 대화창패널 ->그리드...?
-		window.setBackground(Color.GRAY);
-		window.setLayout(new GridLayout(5, 1, 0, 0));
+		container = this.getContentPane(); //Frame
+		container.setLayout(null); //No layout manager
 
-		npcf = new ImageIcon("mom.PNG");
-		tb = new ImageIcon("textBallon_checkm.png");
-		JPanel npc_face = new JPanel() { // 엔피씨 얼굴나오게 할 패널
+		window = new JPanel(); 
+		window.setLayout(new GridLayout(5, 1, 0, 0)); // 5button will be in here
+
+		npcf = new ImageIcon("mom.PNG"); //mom's image
+		tb = new ImageIcon("textBallon_checkm.png"); //what does my mom say
+		
+		npc_face = new JPanel() { // in this panel - mom's face
 			public void paintComponent(Graphics g) {
 				g.drawImage(npcf.getImage(), 3, 0, null);
-				g.drawImage(tb.getImage(), 180, 30, null);
+				g.drawImage(tb.getImage(), 180, 30, null); // - and what does mom say
 			}
 		};
 		;
-		npc_face.setLayout(null);
-
+		
+		npc_face.setLayout(null); //set mom's face panel no layout manager.
+		
+// mission1~mission5 -> make button
 		mission1 = new JButton();
-		mission1.setIcon(new ImageIcon("mission_b.png"));
+		mission1.setIcon(new ImageIcon("mission_b.png")); 
 		mission1.addActionListener(new mission_b_listener());
-		JLabel label1 = new JLabel("            M  i  s  s  i  o  n  1  ");
+		label1 = new JLabel("            M  i  s  s  i  o  n  1  ");
 		mission1.setRolloverIcon(new ImageIcon(""));
 		label1.setFont(font);
-		mission1.add(label1);
+		
 
 		mission2 = new JButton();
 		mission2.setIcon(new ImageIcon("mission_b.png"));
 		mission2.addActionListener(new mission_b_listener());
 		mission2.setRolloverIcon(new ImageIcon(""));
-		JLabel label2 = new JLabel("            M  i  s  s  i  o  n  2  ");
+		label2 = new JLabel("            M  i  s  s  i  o  n  2  ");
 		label2.setFont(font);
-		mission2.add(label2);
+		
 
 		mission3 = new JButton();
 		mission3.setIcon(new ImageIcon("mission_b.png"));
 		mission3.addActionListener(new mission_b_listener());
 		mission3.setRolloverIcon(new ImageIcon(""));
-		JLabel label3 = new JLabel("            M  i  s  s  i  o  n  3  ");
+		label3 = new JLabel("            M  i  s  s  i  o  n  3  ");
 		label3.setFont(font);
-		mission3.add(label3);
+		
 
 		mission4 = new JButton();
 		mission4.setIcon(new ImageIcon("mission_b.png"));
 		mission4.addActionListener(new mission_b_listener());
 		mission4.setRolloverIcon(new ImageIcon(""));
-		JLabel label4 = new JLabel("            M  i  s  s  i  o  n  4  ");
+		label4 = new JLabel("            M  i  s  s  i  o  n  4  ");
 		label4.setFont(font);
-		mission4.add(label4);
 
 		mission5 = new JButton();
 		mission5.setIcon(new ImageIcon("mission_b.png"));
 		mission5.addActionListener(new mission_b_listener());
 		mission5.setRolloverIcon(new ImageIcon(""));
-		JLabel label5 = new JLabel("            M  i  s  s  i  o  n  5  ");
+		label5 = new JLabel("            M  i  s  s  i  o  n  5  ");
 		label5.setFont(font);
-		mission5.add(label5);
+		
+		try{	
+			mission1.add(label1);
+			mission2.add(label2);
+			mission3.add(label3);
+			mission4.add(label4);
+			mission5.add(label5);
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+//make button finish--		
 
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(5, 1, 0, 0));
-		buttonPanel.add(mission1);
-		buttonPanel.add(mission2);
-		buttonPanel.add(mission3);
-		buttonPanel.add(mission4);
-		buttonPanel.add(mission5);
+		
+		//add buttons in window!
+		try{
+			window.add(mission1);
+			window.add(mission2);
+			window.add(mission3);
+			window.add(mission4);
+			window.add(mission5);
+		}
+		catch(NullPointerException e){
+			e.printStackTrace();
+		}
 
-		window.add(mission1);
-		window.add(mission2);
-		window.add(mission3);
-		window.add(mission4);
-		window.add(mission5);
-
+		checkAndexit = new JButton("Ok! Thank you!");// check and exit
+		checkmission = new JPanel(); //check mission!
+		checkmission.setLayout(null);
+		
 		window.setBounds(0, 0, 390, 310);
 		npc_face.setBounds(390, 0, 300, 350);
-
-		container.add(window);
-		container.add(npc_face);
-
-		checkmission = new JPanel();
-		checkmission.setLayout(null);
-
-		checkAndexit = new JButton("Ok! Thank you!");
 		checkAndexit.setBounds(130, 270, 130, 30);
-		checkmission.add(checkAndexit);
+		checkmission.setBounds(0, 0, 390, 310);
+		
+		
 
+		
+
+		
+		
 		mission = new JTextArea();
-		mission.setLineWrap(true);
+		mission.setLineWrap(true); //wrap line to not over block
 		mission.setBounds(20, 10, 350, 250);
-		checkmission.add(mission);
-
-		Border missionBorder = BorderFactory.createLineBorder(Color.black, 3);
-		Border emptyBorder = BorderFactory.createEmptyBorder(7, 7, 7, 7);
+		mission.setFont(font2);
+		
+		
+		try{
+			container.add(window);
+			container.add(npc_face);
+			checkmission.add(mission);
+			checkmission.add(checkAndexit);
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+		
+		missionBorder = BorderFactory.createLineBorder(Color.black, 3);
+		emptyBorder = BorderFactory.createEmptyBorder(7, 7, 7, 7);
+		
 		mission.setBorder(BorderFactory.createCompoundBorder(missionBorder, emptyBorder));
-
-		// Make textArea's Border -> http://blog.naver.com/timberx/30111890732 -
-		// reference
+		// reference --  Make textArea's Border -> http://blog.naver.com/timberx/30111890732
+	
 
 	}
 	
-	class Check implements ActionListener {
+	class Check implements ActionListener { //check mission
 		int k;
-		public Check(int i){
+		public Check(int i){ // id
 			k=i;
 		}
 		public void actionPerformed(ActionEvent event) {
-			if(UserFile.Users.get(idx).getLevel()!=k){
-				JOptionPane.showMessageDialog(null, "FAULT! You aren't in level "+k+"! You lose 500Money:(", "Message", JOptionPane.INFORMATION_MESSAGE);
-				UserFile.Users.get(idx).setMoney(UserFile.Users.get(idx).getMoney()-500);
-				f.fileSave();
-			}else {
-				//미션 제대로 수행했을때와 안했을때!
-				System.out.println("!!");
+			try{
+				if(UserFile.Users.get(idx).getLevel()!=k){ //if your level is low
+					JOptionPane.showMessageDialog(null, "FAULT! You aren't in level "+k+"! You lose 500Money:(", "Message", JOptionPane.INFORMATION_MESSAGE);
+					UserFile.Users.get(idx).setMoney(UserFile.Users.get(idx).getMoney()-500);
+					f.fileSave();
+				}else {
+					//미션 제대로 수행했을때와 안했을때!
+					System.out.println("!!");
+				}
 			}
+			catch(IndexOutOfBoundsException e)
+			{
+				e.printStackTrace();
+			}
+			
 			setVisible(false);
 		}
 	}
+	
+	
 
-	class mission_b_listener implements ActionListener {
+	class mission_b_listener implements ActionListener { //
 		public void actionPerformed(ActionEvent event) {
-
-			m[0] = new Mission1();
-			m[1] = new Mission2();
-			m[2] = new Mission3();
-			m[3] = new Mission4();
-			m[4] = new Mission5();
 			
-			checkmission.setBounds(0, 0, 390, 310);
+			m[0] = new Mission1(); //Mission 1 class
+			m[1] = new Mission2(); //Mission 2 class
+			m[2] = new Mission3(); //Mission 3 class
+			m[3] = new Mission4(); //Mission 4 class
+			m[4] = new Mission5(); //Mission 5 class
+			
+			
 			window.setVisible(false);
-			container.remove(window);
-			mission.setFont(font2);
 			
+			
+			//bring mission text
 			if (event.getSource() == mission1) {
 				mission.setText(((Mission1) m[0]).stringCheck(1));
 				checkAndexit.addActionListener(new Check(1));				
@@ -194,7 +269,16 @@ public class checkFrame extends JFrame {
 				mission.setText(((Mission5) m[4]).stringCheck(5));
 				checkAndexit.addActionListener(new Check(5));
 			}
-			container.add(checkmission);
+			
+			//and show mission text
+			try{
+				container.remove(window);
+				container.add(checkmission);
+			}
+			catch(NullPointerException e)
+			{
+				e.printStackTrace();
+			}
 		}
 	}
 
