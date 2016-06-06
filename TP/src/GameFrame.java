f3e00bab967faa1cc56ae6ceeb0784d1ea95b1eb
김대현 <eogussla12@gmail.com>
2016-06-07 02:30:29 +0900

    asd

--- TP/src/GameFrame.java
+++ TP/src/GameFrame.java
@@ -1,5 +1,8 @@
+import java.awt.Font;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public abstract class GameFrame extends JFrame {
 	/*Crating Labels that have user information*/
@@ -8,6 +11,8 @@ public abstract class GameFrame extends JFrame {
 	private JLabel userORANGE_label = new JLabel();
 	private JLabel userSTRAWBERRY_label = new JLabel();
 	private JLabel userMONEY_label = new JLabel();
+	private JLabel userLEVEL_label = new JLabel();
+	private JLabel userID_label = new JLabel();
 	
 	/* Crate User file*/
 	private UserFile f = new UserFile();
@@ -23,6 +28,47 @@ public abstract class GameFrame extends JFrame {
 		
 	}
 	
+	/* crate Jpanel that have user information Jlebel */
+	JPanel userImfoPane() {
+
+		JPanel menupanel = new JPanel();
+
+		Font font = new Font("Dialog", Font.BOLD, 13); // 20은 글자 크기
+
+		userID_label = new JLabel("User   :   " + UserFile.Users.get(idx).getId());
+		userID_label.setFont(font);
+
+		userMONEY_label.setText("    Money   :   " + UserFile.Users.get(idx).getMoney());
+		userMONEY_label.setFont(font);
+
+		userLEVEL_label = new JLabel("    Level   :   " + UserFile.Users.get(idx).getLevel());
+		userLEVEL_label.setFont(font);
+
+		userAPPLE_label.setText("    Apple  BOX :   " + UserFile.Users.get(idx).getApplebox());
+		userAPPLE_label.setFont(font);
+
+		userGRAPE_label.setText("    Grape  BOX  :   " + UserFile.Users.get(idx).getGrapebox());
+		userGRAPE_label.setFont(font);
+
+		userORANGE_label.setText("    Orange  BOX  :   " + UserFile.Users.get(idx).getOrangebox());
+		userORANGE_label.setFont(font);
+
+		userSTRAWBERRY_label.setText("    Strawberry  BOX  :   " + UserFile.Users.get(idx).getStrawberrybox());
+		userSTRAWBERRY_label.setFont(font);
+
+		changeImfo();
+
+		menupanel.add(userID_label);
+		menupanel.add(userLEVEL_label);
+		menupanel.add(userMONEY_label);
+		menupanel.add(userAPPLE_label);
+		menupanel.add(userGRAPE_label);
+		menupanel.add(userORANGE_label);
+		menupanel.add(userSTRAWBERRY_label);
+
+		return menupanel;
+
+	}
 	/* User information label change */
 	void changeImfo() {
 		userMONEY_label.setText("    Money   :   " + UserFile.Users.get(idx).getMoney());
@@ -30,6 +76,7 @@ public abstract class GameFrame extends JFrame {
 		userGRAPE_label.setText("    Grape  BOX  :   " + UserFile.Users.get(idx).getGrapebox());
 		userORANGE_label.setText("    Orange  BOX  :   " + UserFile.Users.get(idx).getOrangebox());
 		userSTRAWBERRY_label.setText("    Strawberry  BOX  :   " + UserFile.Users.get(idx).getStrawberrybox());
+	
 	}
 
 	
